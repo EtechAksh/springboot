472e79d85040f5315e4c317494b560cfeb629acd
Akshatha.Prabhu <akshatha.prabhu@target.com>
2019-08-26 12:48:09 +0530

    added pom.xml, hello world

---
+++ src/main/java/app/HelloWorld.java
@@ -0,0 +1,41 @@
+package app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@ResponseBody //-- means it will be scanned as a bean
+public class HelloWorld {
+
+    @RequestMapping("/helloWorld")    //{{host}}:8080/helloWorld
+    // @RequestMapping("/")
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+
+
+    //Example of returning an Object
+    @RequestMapping("/student")
+    public Student getStudent() {
+        return new Student(56, "Akshatha");
+    }
+
+
+    //Example of returning a List of objects
+    @RequestMapping("/listOfStudents")
+    public List<Student> getAllStudents() {
+
+        return Arrays.asList(
+            new Student(1, "Neha"),
+            new Student(2, "Rajesh"),
+            new Student(3, "Ankita"),
+            new Student(5, "Mohan"),
+            new Student(6, "Rohan"),
+            new Student(7, "Priyanka")
+        );
+    }
+}
---
+++ src/main/java/app/SpringbootApp.java
@@ -0,0 +1,38 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * A springboot app doesn't need to be deployed separately on a server
+ * but it requires a 'main' method which will be start the servlet container, host the app etc.
+ *
+ * @SpringBootApplication - will tell spring that this class is the starting point for our app */
+@SpringBootApplication
+public class SpringbootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApp.class, args);
+    }
+}
+
+
+
+
+
+/*
+
+-
+ */
+
+
+
+
+//1. About springboot
+// about pom.xml - https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-first-application.html#getting-started-first-application-dependencies
+// start the app without any mapping ---show default error
+// Tomcat started, dispatcherServlet??
+// hello world program   -- @restcontroller vs @controller  - https://javarevisited.blogspot.com/2017/08/difference-between-restcontroller-and-controller-annotations-spring-mvc-rest.html
+//https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-first-application.html#getting-started-first-application-annotations
+
+
